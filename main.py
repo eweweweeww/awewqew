d69397fb8caa37b245b154d59864a9498942db9b
eweweweeww <165377616+eweweweeww@users.noreply.github.com>
2024-04-08 20:57:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import re 
 def resultado(data=response.json()):
-  sssss = re.findall("'streamId': '(23.*?)'", str(data))
+  eusou_um_panda = re.findall("'streamId': '(23.*?)'", str(data))
   if len(sssss) > 0:
-    return sssss
+    return eusou_um_panda
   else:
     return False
     
